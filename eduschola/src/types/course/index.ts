f37cdd4e80ba081cc1a2d402dfd9a95eec8f0710
Darab-gif <darabcode@gmail.com>
2023-08-26 20:38:35 +0100

    created staff and course type definitions

---
+++ eduschola/src/types/course/index.ts
@@ -0,0 +1,7 @@
+export type CourseIT = {
+  course_id: number;
+  name: string;
+  description: string;
+  teacher: string;
+  school: string;
+};
---
+++ eduschola/src/types/staff/index.ts
@@ -0,0 +1,10 @@
+export type StaffIT = {
+  staff_id: number;
+  user: string;
+  phone: number;
+  email: string;
+  address: string;
+  school?: string;
+  subjects: string[];
+  staff_role: string;
+};
